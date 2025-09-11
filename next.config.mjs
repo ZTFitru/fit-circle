aa6a0287afc3af799e9b817cff1d9886ae132382
Zelalem Fitru <zelalemtfitru@gmail.com>
2025-09-10 21:26:45 -0400

    Create: added a domain in my config file.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import { URL } from "url";
+
+const endpoint = process.env.IMAGEKIT_URL_ENDPOINT;
+const hostname = endpoint ? new URL(endpoint).hostname : "ik.imagekit.io";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [hostname],
+  },
+};
 
 export default nextConfig;
